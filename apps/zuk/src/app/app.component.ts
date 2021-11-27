c6b04d20a1f6cfeb9de028593705791650a1cadf
zukkari <stanislav.myshkovski@gmail.com>
2021-11-27 17:30:26 +0200

    Remove nx starting page

--- apps/zuk/src/app/app.component.ts
+++ apps/zuk/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'zukkari.github.io-root',
+  selector: 'zukkari-github-io-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
